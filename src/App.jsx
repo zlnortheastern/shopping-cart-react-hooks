fec6ed586df049ea5252af332df32a6307825709
Zhi Ling <ling.zhi@northeastern.edu>
2024-03-29 14:50:59 -0700

    Fix shopping cart persistence

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,13 @@ import CreateProductForm from "./components/CreateProductForm.jsx";
 import ShoppingCart from "./components/ShoppingCart.jsx";
 import { myFirebase } from "./models/MyFirebase.js";
 
+// Initialize the shopping cart from local storage or an empty array if it doesn't exist
+const initialCart = JSON.parse(localStorage.getItem("shoppingCart")) || [];
+
 export default function App() {
   // Hooks declaration
   const [products, setProducts] = useState([]);
-  const [productsToBuy, setProductsToBuy] = useState([]);
+  const [productsToBuy, setProductsToBuy] = useState(initialCart);
 
   // Get new product form value and store it into firebase
   const onAddProduct = async (product) => {
@@ -30,32 +33,46 @@ export default function App() {
   const onUpdateProduct = async (product) => {
     await myFirebase.updateProduct(product);
     setProducts(products.map((p) => (p.id === product.id ? product : p)));
+
     setProductsToBuy(productsToBuy.map((p) => (p.id === product.id ? { ...product } : p)));
+    storeCartDataLocally(productsToBuy.map((p) => (p.id === product.id ? { ...product } : p)));       
   };
 
   // Perform deletion of product
   const onDeleteProduct = async (product) => {
     await myFirebase.deleteProduct(product.id);
     setProducts(products.filter((p) => p.id !== product.id));
+
+     // Remove product from the shopping cart if it exists
+     setProductsToBuy(productsToBuy.filter((p) => p.id !== product.id));
+
+     // Update local storage
+     localStorage.setItem("shoppingCart", JSON.stringify(productsToBuy.filter((p) => p.id !== product.id)));
   };
 
   // Shopping cart item insertion
   const onAddProductToBuy = (product) => {
     setProductsToBuy([...productsToBuy, product]);
+    storeCartDataLocally([...productsToBuy, product]);
   };
 
   // Shopping cart item deletion
   const onRemoveCart = (index) => {
     setProductsToBuy(productsToBuy.filter((_, i) => i !== index));
+    storeCartDataLocally(productsToBuy.filter((_, i) => i !== index));
   }
 
+  const storeCartDataLocally = (cart) =>{
+    localStorage.setItem("shoppingCart", JSON.stringify(cart));
+  }
+  
   // Load data when component is mounted
   useEffect(() => {
     const getProducts = async () => {
       const products = await myFirebase.getProducts();
       setProducts(products);
     }
-
+    
     getProducts();
   }, []);
 
